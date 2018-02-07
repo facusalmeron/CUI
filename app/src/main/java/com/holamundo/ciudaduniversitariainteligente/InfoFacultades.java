c9323b9708adbb3f12fa56381f90baa9a1346cf9
Facu Salmerón <Facu Salmern>
2018-02-07 18:05:53 -0300

    1

---
+++ app/src/main/java/com/holamundo/ciudaduniversitariainteligente/InfoFacultades.java
@@ -0,0 +1,132 @@
+package com.holamundo.ciudaduniversitariainteligente;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Created by usuario on 7/1/2018.
+ */
+
+public class InfoFacultades extends Fragment {
+
+    private Spinner spinnerBusqueda = null;
+    private Button botonBusqueda = null;
+    private MainActivity oMainActivity = null;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final View rootView = inflater.inflate(R.layout.info_facultades, container, false);
+
+        spinnerBusqueda = (Spinner) rootView.findViewById(R.id.busquedaSpinnerInfo);
+        botonBusqueda = (Button) rootView.findViewById(R.id.boton_busquedaInfo);
+        oMainActivity = (MainActivity) getActivity();
+
+        String[] itemsSB1 = {" --- ", "FICH", "FBCB", "FCM", "FADU", "FHUC", "ISM", "FCE"};
+        ArrayAdapter<String> arraySB1 = new ArrayAdapter<String>(getActivity(), R.layout.spinner_layout, itemsSB1);
+        spinnerBusqueda.setAdapter(arraySB1);
+
+        final ImageView imagenFacultad = (ImageView) rootView.findViewById(R.id.imagenFacultad);
+        imagenFacultad.setImageResource(R.drawable.info);
+        final TextView nombreFacultad = (TextView) rootView.findViewById(R.id.nombreFacultad);
+        final TextView localidadFacultad = (TextView) rootView.findViewById(R.id.localidadFacultad);
+        final TextView direccionFacultad = (TextView) rootView.findViewById(R.id.direccionFacultad);
+        final TextView telefonoFacultad = (TextView) rootView.findViewById(R.id.telefonoFacultad);
+        final TextView faxFacultad = (TextView) rootView.findViewById(R.id.faxFacultad);
+        final TextView paginaFacultad = (TextView) rootView.findViewById(R.id.paginaFacultad);
+
+        botonBusqueda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (!spinnerBusqueda.getSelectedItem().toString().equals(" --- ")) {
+                    switch (spinnerBusqueda.getSelectedItem().toString()){
+                        case "FICH":
+                            imagenFacultad.setImageResource(R.drawable.fich_info);
+                            nombreFacultad.setText("Facultad de Ingeniería y Ciencias Hídricas");
+                            localidadFacultad.setText("(3000) Santa Fe - Argentina");
+                            direccionFacultad.setText("Ciudad Universitaria. Ruta Nacional N° 168 - Km 472,4");
+                            telefonoFacultad.setText("Tel.:(+54) (0342) 4575233 / 34 / 39 / 44 / 45");
+                            faxFacultad.setText("Fax:(+54) (0342) 457-5224");
+                            paginaFacultad.setText("http://fich.unl.edu.ar/");
+                            break;
+                        case "FBCB":
+                            imagenFacultad.setImageResource(R.drawable.fbcb_info);
+                            nombreFacultad.setText("Facultad de Bioquímica y Ciencias Biológicas");
+                            localidadFacultad.setText("(3000) Santa Fe - Argentina");
+                            direccionFacultad.setText("Ciudad Universitaria. Ruta Nacional N° 168 - Km 472,4");
+                            telefonoFacultad.setText("Tel.: +54 (342) 4575215 /4575216");
+                            faxFacultad.setText("Fax: +54 (342) 4575216");
+                            paginaFacultad.setText("http://www.fbcb.unl.edu.ar/");
+                            break;
+                        case "FCM":
+                            imagenFacultad.setImageResource(R.drawable.fcm_info);
+                            nombreFacultad.setText("Facultad de Ciencias Médicas");
+                            localidadFacultad.setText("(3000) Santa Fe - Argentina");
+                            direccionFacultad.setText("Ciudad Universitaria. Ruta Nacional N° 168 - Km 472,4");
+                            telefonoFacultad.setText("Tel: (54) 0342 - 4575116/7");
+                            faxFacultad.setText("Fax: -----");
+                            paginaFacultad.setText("http://www.fcm.unl.edu.ar/");
+                            break;
+                        case "FADU":
+                            imagenFacultad.setImageResource(R.drawable.fadu_info);
+                            nombreFacultad.setText("Facultad de Arquitectura, Diseño y Urbanismo");
+                            localidadFacultad.setText("(3000) Santa Fe - Argentina");
+                            direccionFacultad.setText("Ciudad Universitaria. Ruta Nacional N° 168 - Km 472,4");
+                            telefonoFacultad.setText("Tel: +54 (342) 4575100 ");
+                            faxFacultad.setText("Fax: +54 (342) 4575112");
+                            paginaFacultad.setText("http://www.fadu.unl.edu.ar/");
+                            break;
+                        case "FHUC":
+                            imagenFacultad.setImageResource(R.drawable.fhuc_info);
+                            nombreFacultad.setText("Facultad de Humanidades y Ciencias");
+                            localidadFacultad.setText("(3000) Santa Fe - Argentina");
+                            direccionFacultad.setText("Ciudad Universitaria. Ruta Nacional N° 168 - Km 472,4");
+                            telefonoFacultad.setText("Tel: +54 (0342) 4575105");
+                            faxFacultad.setText("Fax: +54 (0342) 4575105");
+                            paginaFacultad.setText("http://www.fhuc.unl.edu.ar/");
+                            break;
+                        case "ISM":
+                            imagenFacultad.setImageResource(R.drawable.ism_info);
+                            nombreFacultad.setText("Instituto Superior de Música");
+                            localidadFacultad.setText("(3000) Santa Fe - Argentina");
+                            direccionFacultad.setText("Ciudad Universitaria. Ruta Nacional N° 168 - Km 472,4");
+                            telefonoFacultad.setText("Tel: +54 (0342) 4511622/3");
+                            faxFacultad.setText("Fax: ----------");
+                            paginaFacultad.setText("http://www.ism.unl.edu.ar/");
+                            break;
+                        case "FCE":
+                            imagenFacultad.setImageResource(R.drawable.fce_info);
+                            nombreFacultad.setText("Facultad de Ciencias Económicas");
+                            localidadFacultad.setText("(3000) Santa Fe - Argentina");
+                            direccionFacultad.setText("Moreno 2557");
+                            telefonoFacultad.setText("Tel: (54) 0342 - 4571180 / 4571181");
+                            faxFacultad.setText("Fax: +54 (342) 4551222");
+                            paginaFacultad.setText("http://www.fce.unl.edu.ar/");
+                            break;
+                    }
+                }
+                else{
+                    imagenFacultad.setImageResource(R.drawable.info);
+                    nombreFacultad.setText("");
+                    localidadFacultad.setText("");
+                    direccionFacultad.setText("");
+                    telefonoFacultad.setText("");
+                    faxFacultad.setText("");
+                    paginaFacultad.setText("");
+                    Toast.makeText(getActivity(), "Primero debe ingresar la Facultad a buscar",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+    return rootView;
+}
+}
---
+++ app/src/main/java/com/holamundo/ciudaduniversitariainteligente/MainActivity.java
@@ -0,0 +1,414 @@
+package com.holamundo.ciudaduniversitariainteligente;
+
+
+import android.Manifest;
+import android.app.Activity;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.support.v4.app.FragmentManager;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Vector;
+
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+
+    private static final String[] INITIAL_PERMS = new String[]{
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+    };
+
+    private static final int INITIAL_REQUEST = 1337;
+
+
+    /* Atributos de la clase*/
+    private ArmaCamino oArmaCamino = null;
+    private MapsFragment mapsFragment = null;
+    private FragmentManager fm = getSupportFragmentManager();
+    private FloatingActionButton qrBoton = null;
+    private TextView textClima = null;
+    private IntentIntegrator scanIntegrator = new IntentIntegrator(this);
+    private ultimasBusquedas ultimasBusquedas = null;
+    private Menu menu = null;
+    private BaseDatos CUdb = null;
+
+    /*Funciones*/
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Ciudad Inteligente");
+        setSupportActionBar(toolbar);
+
+        //Instancio la base de datos
+        CUdb = new BaseDatos(getApplicationContext(), "DBCUI", null, 27);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(INITIAL_PERMS, INITIAL_REQUEST);
+        }
+
+        //Instancio los objetos para ArmaCamino y el MapFragment
+        oArmaCamino = new ArmaCamino(this);
+        mapsFragment = new MapsFragment();
+        ultimasBusquedas = new ultimasBusquedas();
+        ultimasBusquedas.setMainActivity(this);
+
+        //Agrego Nodos a mi vector de nodos en oArmaCamino
+        cargaNodos();
+
+        //Boton Flotante que está abajo a la derecha, para leer QR
+        qrBoton = (FloatingActionButton) findViewById(R.id.fab);
+        qrBoton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Se procede con el proceso de scaneo
+                scanIntegrator.initiateScan();
+            }
+        });
+
+        //Agrego TextView abajo trayendo datos del webService del clima
+        textClima = (TextView) findViewById(R.id.textWSClima);
+        new HttpAsyncTask().execute("http://api.wunderground.com/api/e8cd515cb766a5bf/lang:SP/forecast/geolookup/q/-31.6182466,-60.7013970,15.json");
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        //Cambio el fragment por defecto por mi mapFragment
+        fm.beginTransaction().replace(R.id.fragment_container, mapsFragment).addToBackStack(null).commit();
+    }
+
+    public static String GET(String url) {
+        InputStream inputStream = null;
+        String result = "";
+        try {
+
+            // create HttpClient
+            HttpClient httpclient = new DefaultHttpClient();
+
+            // make GET request to the given URL
+            HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
+
+            // receive response as inputStream
+            inputStream = httpResponse.getEntity().getContent();
+
+            // convert inputstream to string
+            if (inputStream != null)
+                result = convertInputStreamToString(inputStream);
+            else
+                result = "Did not work!";
+
+        } catch (Exception e) {
+            Log.d("InputStream", e.getLocalizedMessage());
+        }
+
+        return result;
+    }
+
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while ((line = bufferedReader.readLine()) != null)
+            result += line;
+
+        inputStream.close();
+        return result;
+
+    }
+
+    private class HttpAsyncTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            return GET(urls[0]);
+        }
+
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            try {
+                JSONObject json = new JSONObject(result); //Convierte String a JSONObject
+
+                String diaSemana = json.getJSONObject("forecast").
+                        getJSONObject("simpleforecast").
+                        getJSONArray("forecastday").
+                        getJSONObject(0).
+                        getJSONObject("date").getString("weekday_short");
+
+                String dia = json.getJSONObject("forecast").
+                        getJSONObject("simpleforecast").
+                        getJSONArray("forecastday").
+                        getJSONObject(0).
+                        getJSONObject("date").getString("day");
+
+                String mes = json.getJSONObject("forecast").
+                        getJSONObject("simpleforecast").
+                        getJSONArray("forecastday").
+                        getJSONObject(0).
+                        getJSONObject("date").getString("monthname_short");
+
+                String temperatura = json.getJSONObject("forecast").
+                        getJSONObject("simpleforecast").
+                        getJSONArray("forecastday").getJSONObject(0).getJSONObject("high").getString("celsius");
+
+                String ubicacion = json.getJSONObject("location").getString("city");
+
+                textClima.setText(diaSemana + " " + dia + " " + mes + " - " + temperatura + "ºC - " + ubicacion);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            if (fm.findFragmentById(R.id.fragment_container) instanceof MapsFragment) {
+                finish();
+            } else {
+                fm.popBackStack();
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        this.menu = menu;
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        for (int i = 0; i < menu.size(); i++) {
+            if (menu.getItem(i).getTitle().charAt(0) == '*') {
+                menu.getItem(i).setTitle(menu.getItem(i).getTitle().toString().substring(1));
+                item.setTitle("*" + item.getTitle());
+                break;
+            }
+        }
+        /*Si estoy mostrando una polilinea, la cambio segun la opcion de piso seleccionada*/
+        if (mapsFragment.modoPolilinea()) {
+            if (item.toString().contains("Baja")) {
+                mapsFragment.cambiarPolilinea(0);
+                return true;
+            } else {
+                mapsFragment.cambiarPolilinea(Integer.parseInt(item.toString().substring(item.toString().indexOf(' ') + 1)));
+            }
+        }
+        /*Esto es si estoy mostrando nodos*/
+        else {
+            if (item.toString().contains("Baja")) {
+                mapsFragment.cambiarNodos(0);
+                return true;
+            } else {
+                mapsFragment.cambiarNodos(Integer.parseInt(item.toString().substring(item.toString().indexOf(' ') + 1)));
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    //Switch segun en que opcion del menu desplegable se selecciona
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.buscar) {
+            if (!(fm.findFragmentById(R.id.fragment_container) instanceof Busqueda)) {
+                mapsFragment.limpiarMapa();
+                qrBoton.hide();
+                menu.clear();
+                Busqueda busqueda = new Busqueda();
+                //fm.popBackStack();
+                fm.beginTransaction().replace(R.id.fragment_container, busqueda).addToBackStack(null).commit();
+            }
+
+        } else if (id == R.id.mapa_completo) {
+            mapsFragment.limpiarMapa();
+            menu.clear();
+            if (!(fm.findFragmentById(R.id.fragment_container) instanceof MapsFragment)) {
+                qrBoton.show();
+                fm.beginTransaction().replace(R.id.fragment_container, mapsFragment).addToBackStack(null).commit();
+            }
+
+        } else if (id == R.id.ultimas) {
+            if (!(fm.findFragmentById(R.id.fragment_container) instanceof ultimasBusquedas)) {
+                qrBoton.hide();
+                menu.clear();
+                //fm.popBackStack();
+                fm.beginTransaction().replace(R.id.fragment_container, ultimasBusquedas).addToBackStack(null).commit();
+            }
+
+        } else if (id == R.id.infoFacultades){
+            if (!(fm.findFragmentById(R.id.fragment_container) instanceof InfoFacultades)) {
+                qrBoton.hide();
+                menu.clear();
+                InfoFacultades infoFacultades = new InfoFacultades();
+                fm.beginTransaction().replace(R.id.fragment_container, infoFacultades).commit();
+
+            }
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    /*
+    Mostrar busqueda llama a las funciones del mapFragment que:
+    -muestran un conjunto de puntos en el mapa
+    -muestran una polilinea desde el punto mas cercano hasta el objetivo
+    *setPuntoMasCercano setea en oArmaCamino el nodo mas cercano a la posición donde estoy parado
+    Luego reemplazo el fragment de Busqueda por el de mapa
+    */
+    public void mostrarBusqueda(String Edificio, String Nombre) {
+        mapsFragment.setPisoActual(0);
+        if (Edificio.equals("*")) {
+            mapsFragment.mostrarNodos(oArmaCamino.nodosMapa(Nombre));
+            menu.clear();
+            menu.add("Planta Baja");
+            for (int i = 1; i < mapsFragment.getCantPisos(); i++) {
+                menu.add("Piso " + i);
+            }
+            menu.getItem(mapsFragment.getPisoActual()).setTitle("*" + menu.getItem(mapsFragment.getPisoActual()).getTitle());
+            getMenuInflater().inflate(R.menu.main, menu);
+        } else {
+            oArmaCamino.setPuntoMasCercano(mapsFragment.getPosicion(), mapsFragment.getPisoActual());
+            mapsFragment.dibujaCamino(oArmaCamino.camino(Edificio, Nombre));
+            menu.clear();
+            menu.add("Planta Baja");
+            for (int i = 1; i < mapsFragment.getCantPisos(); i++) {
+                menu.add("Piso " + i);
+            }
+            menu.getItem(mapsFragment.getPisoActual()).setTitle("*" + menu.getItem(mapsFragment.getPisoActual()).getTitle());
+            getMenuInflater().inflate(R.menu.main, menu);
+            String texto = "Su objetivo está en " + oArmaCamino.getPisoObjetivo();
+            Toast.makeText(getApplicationContext(), texto, Toast.LENGTH_LONG).show();
+        }
+        fm.beginTransaction().replace(R.id.fragment_container, mapsFragment).addToBackStack(null).commit();
+        qrBoton.show();
+    }
+
+    //Funcion que le pasa a oArmaCamino un edificio y devuelve un Vector con todas las aulas de ese edificio
+    public Vector<Punto> verAulasPorEdificio(String Edificio) {
+        return oArmaCamino.verAulasPorEdificio(Edificio);
+    }
+
+    //Funcion que le pasa a oArmaCamino un edificio y devuelve un Vector con todas las oficinas de ese edificio
+    public Vector<Punto> verOficinasPorEdificio(String Edificio) {
+        return oArmaCamino.verOficinasPorEdificio(Edificio);
+    }
+
+    //Funcion que le pasa a oArmaCamino un edificio y devuelve un Vector con todas los laboratorios de ese edificio
+    public Vector<Punto> verLaboratoriosPorEdificio(String Edificio) {
+        return oArmaCamino.verLaboratoriosPorEdificio(Edificio);
+    }
+
+
+    //Funcion para crear nodos del mapa y sus conexiones
+    private void cargaNodos() {
+        Vector<Punto> puntos = new Vector<>();
+        SQLiteDatabase db1 = CUdb.getReadableDatabase();
+        Cursor c = db1.rawQuery("SELECT *  FROM Punto", null);
+        c.moveToFirst();
+
+        //Creo y agrego los nodos
+        if (c.getCount() > 0) {
+            do {
+                Punto oPunto = new Punto(c.getInt(0), Double.parseDouble(c.getString(1)), Double.parseDouble(c.getString(2)), c.getString(3), c.getInt(4), c.getString(5), c.getInt(6));
+                puntos.add(oPunto);
+            } while (c.moveToNext());
+        }
+
+        //Genero las conexiones
+        for (int i = 0; i < puntos.size(); i++) {
+            Cursor d = db1.rawQuery("SELECT idHasta FROM Conexiones WHERE idDesde = " + puntos.elementAt(i).getId(), null);
+            d.moveToFirst();
+            if (d.getCount() > 0) {
+                do {
+                    puntos.elementAt(i).addVecino(puntos.elementAt(d.getInt(0)));
+                } while (d.moveToNext());
+            }
+            oArmaCamino.addNodo(puntos.elementAt(i));
+        }
+        //Cierro DB
+        puntos.clear();
+        db1.close();
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        //Se obtiene el resultado del proceso de scaneo y se parsea
+        IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (scanningResult != null) {
+            //Quiere decir que se obtuvo resultado pro lo tanto:
+            //Desplegamos en pantalla el contenido del código de barra scaneado
+            String scanContent = scanningResult.getContents();
+            mapsFragment.setLat(Double.parseDouble(scanContent.toString().substring(0, (scanContent.toString().indexOf(',')))));
+            mapsFragment.setLon(Double.parseDouble(scanContent.toString().substring((scanContent.toString().indexOf(',')) + 1, scanContent.length() - 2)));
+            mapsFragment.setPisoActual(Integer.parseInt(scanContent.toString().substring(scanContent.toString().length() - 1)));
+            mapsFragment.actualizaPosicion();
+
+            //Actualizo el * del menu de pisos cuando cambio el piso por QR
+            if (mapsFragment.getPisoActual() + 1 <= mapsFragment.getCantPisos()) {
+                for (int i = 0; i < menu.size(); i++) {
+                    if (menu.getItem(i).getTitle().charAt(0) == '*') {
+                        menu.getItem(i).setTitle(menu.getItem(i).getTitle().toString().substring(1));
+                        menu.getItem(mapsFragment.getPisoActual()).setTitle("*" + menu.getItem(mapsFragment.getPisoActual()).getTitle());
+                        break;
+                    }
+                }
+            }
+        } else {
+            //Quiere decir que NO se obtuvo resultado
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "No se ha recibido datos del scaneo!", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+}
